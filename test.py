9f773c92f06fa997d1104ebe395017a09a413da1
James Watson <jameywatson134@gmail.com>
2023-06-18 18:43:27 -0700

    Modularized User_CRUD commit

---
+++ test.py
@@ -0,0 +1,12 @@
+contact = {
+    "name": "james",
+    "nickname": "bond"
+}
+a = 1
+b =2
+            
+sampleList = [ 'items', 'tee', 34, ]
+
+# print(f"his name is {contact.name}, {a+b}")
+# print(contact['name'])
+print(sampleList[0])
